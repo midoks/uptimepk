8a607e3c40c791b2f1250173b46286f517f55f46
dami <midoks@163.com>
2026-04-21 12:37:20 +0800

    update

--- internal/app/handles/handles.go
+++ internal/app/handles/handles.go
@@ -16,10 +16,8 @@ func AdminPage(c *gin.Context) {
 }
 
 func Home(c *gin.Context) {
-	// err := op.AddLog(1, "测试")
-	// fmt.Println(err)
 	data := common.CommonVer(c)
-	c.HTML(http.StatusOK, "backend/admin/index.tmpl", data)
+	c.HTML(http.StatusOK, "home/index.tmpl", data)
 }
 
 func NotFound(c *gin.Context) {
