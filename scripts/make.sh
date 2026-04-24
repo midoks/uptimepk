2b8d0a289470adccb19ae4bc91042ad63eb0488f
dami <midoks@163.com>
2026-04-24 12:21:37 +0800

    Update make.sh

--- scripts/make.sh
+++ scripts/make.sh
@@ -4,12 +4,14 @@ _os=`uname`
 _path=`pwd`
 _dir=`dirname $_path`
 
+sed "s:{APP_PATH}:${_dir}:g" $_dir/scripts/init.d/uptimepk.tpl > $_dir/scripts/init.d/uptimepk
+chmod +x $_dir/scripts/init.d/uptimepk
 
-sed "s:{APP_PATH}:${_dir}:g" $_dir/scripts/init.d/uptimepk.service.tpl > $_dir/scripts/init.d/imail.service
+sed "s:{APP_PATH}:${_dir}:g" $_dir/scripts/init.d/uptimepk.service.tpl > $_dir/scripts/init.d/uptimepk.service
 
 if [ -d /etc/init.d ];then
-	cp $_dir/scripts/init.d/imail /etc/init.d/imail
-	chmod +x /etc/init.d/imail
+	cp $_dir/scripts/init.d/uptimepk /etc/init.d/uptimepk
+	chmod +x /etc/init.d/uptimepk
 fi
 
 echo `dirname $_path`
