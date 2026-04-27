fac096a53a42bc165ea6c2722fc8905a7ed0eaf6
dami <midoks@163.com>
2026-04-27 10:53:42 +0800

    Update init_clean.go

--- internal/op/init_clean.go
+++ internal/op/init_clean.go
@@ -22,7 +22,8 @@ func InitCleanTask() {
 			// time.Sleep(10 * time.Second)
 			// 执行清理
 			if err := CleanExpiredMonitorLogs(); err != nil {
-				SysLog(fmt.Sprintf("[%s] 清理过期监控日志失败: %v", time.Now().Format("2006-01-02 15:04:05"), err))
+				fmt.Printf("[%s] 清理过期监控日志失败: %v\n", time.Now().Format("2006-01-02 15:04:05"), err)
+				SysLog(fmt.Sprintf("清理过期监控日志失败: %v", err))
 			}
 		}
 	}()
